133918fb8c15807c8d2ee914b76a056be81d5475
Jose Roa <jg.roac@gmail.com>
2019-02-15 13:37:39 +0100

    feat: add calculateLoan utility

---
+++ src/utils/calculateLoan.js
@@ -0,0 +1,43 @@
+
+/**
+ * It calculates the repayment schedule for a loan
+ *
+ * @param {Number} amount amount of the loan
+ * @param {Number} duration loan duration in months
+ * @param {Number} rate interest rate
+ */
+const calculateLoan = (
+    amount,
+    duration,
+    rate
+) => {
+    const parsedAmount = Number(amount)
+    const parsedDuration = Number(duration)
+    const parsedRate = Number(rate)
+    const areInvalidParams = isNaN(Number(parsedAmount))
+        || isNaN(parsedDuration)
+        || isNaN(parsedRate)
+
+    if (areInvalidParams) {
+        throw new Error('All the parameters should be numbers');
+    }
+
+    const months = [...Array(duration)].map((u, i) => i)
+    const originalLoan = parsedAmount / parsedDuration;
+    let residualAmount = parsedAmount
+
+    const schedule = months.map((index) => {
+        const interest = Math.round(residualAmount * (parsedRate / 100))
+        residualAmount -= originalLoan
+        return {
+            month: index + 1,
+            original: originalLoan,
+            interest,
+            monthlyRepayment: interest + originalLoan
+        }
+    })
+
+    return schedule;
+}
+
+export default calculateLoan;
